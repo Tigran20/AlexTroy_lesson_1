ce4e33bbab2345a2dff575b328828c5f1198000d
Tigran20 <Podliza122@bk.ru>
2018-03-03 10:11:19 +0300

    lesson1

---
+++ app/src/paid/java/com/alextroy/android/alextroylesson1/MainActivityPaid.kt
@@ -0,0 +1,12 @@
+package com.alextroy.android.alextroylesson1
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityPaid : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_paid)
+    }
+}
